fe5d87ca383ecbce12539a382ef2f6df48e17e4f
ossama21 <ossamahattan@gmail.com>
2024-02-23 16:24:16 +0100

    Day_6_7

---
+++ DAY-1_TO_DAY-15/Day_6.py
@@ -0,0 +1,14 @@
+'''The robot want to eat the cake help him !!'''
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+while not at_goal():
+    if front_is_clear():
+        move()
+    elif right_is_clear():
+        turn_right()
+        move()
+    else:
+        turn_left()
